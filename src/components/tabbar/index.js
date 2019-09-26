f9441b79756b16e6f10b50c472bac656f5b83bbc
Jingle <2521019626@qq.com>
2019-09-26 16:01:10 +0800

    修改tabbar的bug

--- src/components/tabbar/index.js
+++ src/components/tabbar/index.js
@@ -6,11 +6,16 @@ class Tabber extends Component {
     render() {
         return (
             <ul className={style.tabber}>
-                <li><NavLink to="/home" activeClassName={style.active}>首页</NavLink></li>
-                <li><NavLink to="/classify" activeClassName={style.active}>分类</NavLink></li>
-                <li><NavLink to="/cart" activeClassName={style.active}>购物车</NavLink></li>
-                <li><NavLink to="/message" activeClassName={style.active}>消息</NavLink></li>
-                <li><NavLink to="/mine" activeClassName={style.active}>我</NavLink></li>
+                <li><NavLink to="/home" activeClassName={style.active} replace>
+                    <i className={"iconfont icon-shouye"}></i>首页</NavLink></li>
+                <li><NavLink to="/classify" activeClassName={style.active} replace>
+                    <i className={"iconfont icon-fenlei"}></i>分类</NavLink></li>
+                <li><NavLink to="/cart" activeClassName={style.active} replace>
+                    <i className={"iconfont icon-gouwuche"}></i>购物车</NavLink></li>
+                <li><NavLink to="/message" activeClassName={style.active} replace>
+                    <i className={"iconfont icon-xiaoxi"}></i>消息</NavLink></li>
+                <li><NavLink to="/mine" activeClassName={style.active} replace>
+                    <i className={"iconfont icon-wo"}></i>我</NavLink></li>
             </ul>
         );
     }
